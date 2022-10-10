69c528801daff298deebdde7fb369028563bcf4f
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-10-09 19:27:36 -0700

    initial fetch

--- src/App.js
+++ src/App.js
@@ -5,7 +5,28 @@ import Photo from './Photo'
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
+
+
+
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async() => {
+    setLoading(true)
+    let url;
+    url = `${mainUrl} ? client_id=HpuM4GBbigr26Ao64Vq6aCJnOUDvqOx49JXCZhbZXTk`
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+    fetchImages()
+  }, [])
   return <h2>stock photos starter</h2>
 }
 
