fd51d2d32b7da66b374a34946a02510d9ba31888
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2022-10-11 09:38:56 -0700

    search query

--- src/App.js
+++ src/App.js
@@ -14,17 +14,32 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
   const [page,setPage] = useState(1);
+  const [query,setQuery] = useState('')
 
   const fetchImages = async() => {
     setLoading(true)
     let url;
     const urlPage = `&page=${page}`
+    const urlQuery = `&query=${query}`
+
+    if(query){
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+    }
+    else{
+      url = `${mainUrl}${clientID}${urlPage}`
+    }
+
     url = `${mainUrl}${clientID}${urlPage}`
     try {
       const response = await fetch(url)
       const data = await response.json()
       setPhotos((oldPhotos) => {
-        return [...oldPhotos, ...data]
+        if(query){
+          return [...oldPhotos, ...data.results]
+        }
+        else{
+          return [...oldPhotos, ...data]
+        }
       })
       setLoading(false)
     } catch (error) {
@@ -55,7 +70,13 @@ function App() {
     <main>
       <section className="search">
         <form className="search-form">
-          <input type='text' placeholder='search' className='form-input' />
+          <input 
+            type='text' 
+            placeholder='search' 
+            className='form-input' 
+            value={query} 
+            onChange={(e) => setQuery(e.target.value) } 
+          />
           <button type='submit' className='submit-btn' onClick={handleSubmit}>
             <FaSearch />
           </button>
